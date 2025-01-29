a6ead23d1e662b962264f7d961ac66318ed18a0f
Alexandre Costantini <alexandre.costantini@exoalpha.com>
2025-01-29 23:03:02 +0100

    initial commit

---
+++ datetime.go
@@ -0,0 +1,148 @@
+package datetime
+
+import (
+	"fmt"
+	"time"
+)
+
+const (
+	Day      = 24 * time.Hour
+	maxMonth = 11
+)
+
+func SimpleFormat(t time.Time) string {
+	return t.UTC().Format("2006-01-02 15:04:05")
+}
+
+func FormatRFC3339(t time.Time) string {
+	return t.UTC().Format("2006-01-02T15:04:05.000Z")
+}
+
+func ParseRFC3339(str string) (time.Time, error) {
+	t, err := time.Parse("2006-01-02T15:04:05.000Z", str)
+	if err != nil {
+		err = fmt.Errorf("failed to parse datetime '%s': %w", str, err)
+	}
+	return t, err
+}
+
+// MidNightUTC returns the beining of day for the given date.
+func MidNightUTC(date time.Time) time.Time {
+	return date.UTC().Truncate(Day)
+}
+
+// IsMidNightUTC returns true if the given date and time is in the midnight hour.
+func IsMidNightUTC(date time.Time) bool {
+	return date.UTC().Truncate(time.Hour).Hour() == 0
+}
+
+// LastMonth returns the first day of the last month for the given date.
+func LastMonth(t time.Time) time.Time {
+	d := t.UTC()
+	year := d.Year()
+	month := (d.Month() - 1)
+	month--
+	if month < 0 {
+		month += 12
+		year--
+	}
+	return time.Date(year, month+1, 1, 0, 0, 0, 0, time.UTC)
+}
+
+// ThisMonth returns the first day of the current month for the given date.
+func ThisMonth(t time.Time) time.Time {
+	d := t.UTC()
+	year := d.Year()
+	month := d.Month()
+	return time.Date(year, month, 1, 0, 0, 0, 0, time.UTC)
+}
+
+// NextMonth returns the first day of the next month for the given date.
+func NextMonth(t time.Time) time.Time {
+	d := t.UTC()
+	year := d.Year()
+	month := (d.Month() - 1)
+	month++
+	if month > maxMonth {
+		month -= 12
+		year++
+	}
+	return time.Date(year, month+1, 1, 0, 0, 0, 0, time.UTC)
+}
+
+// LastQuarter returns the first day of last quarter for the given date.
+func LastQuarter(t time.Time) time.Time {
+	d := t.UTC()
+	year := d.Year()
+	month := (d.Month() - 1)
+	month -= month % 3
+	month -= 3
+	if month < 0 {
+		month += 12
+		year--
+	}
+	return time.Date(year, month+1, 1, 0, 0, 0, 0, time.UTC)
+}
+
+// ThisQuarter returns the first day of the current quarter for the given date.
+func ThisQuarter(t time.Time) time.Time {
+	d := t.UTC()
+	year := d.Year()
+	month := (d.Month() - 1)
+	month -= month % 3
+	return time.Date(year, month+1, 1, 0, 0, 0, 0, time.UTC)
+}
+
+// NextQuarter returns the first day of the next quarter for the given date.
+func NextQuarter(t time.Time) time.Time {
+	d := t.UTC()
+	year := d.Year()
+	month := (d.Month() - 1)
+	month -= month % 3
+	month += 3
+	if month > maxMonth {
+		month -= 12
+		year++
+	}
+	return time.Date(year, month+1, 1, 0, 0, 0, 0, time.UTC)
+}
+
+// LastYear returns the first day of last year for the given date.
+func LastYear(t time.Time) time.Time {
+	d := t.UTC()
+	year := d.Year() - 1
+	return time.Date(year, time.January, 1, 0, 0, 0, 0, time.UTC)
+}
+
+// ThisYear returns the first day of the current year for the given date.
+func ThisYear(t time.Time) time.Time {
+	d := t.UTC()
+	year := d.Year()
+	return time.Date(year, time.January, 1, 0, 0, 0, 0, time.UTC)
+}
+
+// NextYear returns the first day of the next year for the given date.
+func NextYear(t time.Time) time.Time {
+	d := t.UTC()
+	year := d.Year() + 1
+	return time.Date(year, time.January, 1, 0, 0, 0, 0, time.UTC)
+}
+
+// IsBeginingMonth returns true if t correspond to the first day of a month.
+func IsBeginingMonth(t time.Time) bool {
+	return (t.Day() == 1)
+}
+
+// IsBeginingQuarter returns true if t correspond to the first day of a quarter.
+func IsBeginingQuarter(t time.Time) bool {
+	return (t.Day() == 1 &&
+		(t.Month() == time.January ||
+			t.Month() == time.April ||
+			t.Month() == time.July ||
+			t.Month() == time.October))
+}
+
+// IsBeginingYear returns true if t correspond to the first day of a year.
+func IsBeginingYear(t time.Time) bool {
+	return (t.Day() == 1 && t.Month() == time.January)
+}
---
+++ datetime_test.go
@@ -0,0 +1,242 @@
+package datetime_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/alex-cos/datetime"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSimpleFormat(t *testing.T) {
+	t.Parallel()
+
+	date := time.Date(2020, time.February, 14, 12, 12, 12, 0, time.Local)
+	expected := "2020-02-14 11:12:12"
+	res := datetime.SimpleFormat(date)
+	assert.Equal(t, expected, res)
+}
+
+func TestFormatRFC3339(t *testing.T) {
+	t.Parallel()
+
+	date := time.Date(2020, time.February, 14, 12, 12, 12, 0, time.UTC)
+	expected := "2020-02-14T12:12:12.000Z"
+	res := datetime.FormatRFC3339(date)
+	assert.Equal(t, expected, res)
+}
+
+func TestParseRFC3339(t *testing.T) {
+	t.Parallel()
+
+	date := "2020-02-14T12:12:12.000Z"
+	expected := time.Date(2020, time.February, 14, 12, 12, 12, 0, time.UTC)
+	res, err := datetime.ParseRFC3339(date)
+	assert.NoError(t, err)
+	assert.Equal(t, expected, res)
+
+	date = "2020-02-14 11:12:12"
+	_, err = datetime.ParseRFC3339(date)
+	assert.Error(t, err)
+}
+
+func TestMidNightUTC(t *testing.T) {
+	t.Parallel()
+
+	date := time.Date(2020, time.February, 14, 12, 12, 12, 0, time.Local)
+	expected := time.Date(2020, time.February, 14, 0, 0, 0, 0, time.UTC)
+	res := datetime.MidNightUTC(date)
+	assert.Equal(t, expected, res)
+
+	date = time.Date(2020, time.February, 14, 0, 35, 0, 0, time.Local)
+	expected = time.Date(2020, time.February, 13, 0, 0, 0, 0, time.UTC)
+	res = datetime.MidNightUTC(date)
+	assert.Equal(t, expected, res)
+
+	date = time.Date(2020, time.February, 14, 0, 35, 0, 0, time.UTC)
+	expected = time.Date(2020, time.February, 14, 0, 0, 0, 0, time.UTC)
+	res = datetime.MidNightUTC(date)
+	assert.Equal(t, expected, res)
+}
+
+func TestIsMidNightUTC(t *testing.T) {
+	t.Parallel()
+
+	date := time.Date(2020, time.February, 14, 12, 12, 12, 0, time.Local)
+	res := datetime.IsMidNightUTC(date)
+	assert.False(t, res)
+
+	date = time.Date(2020, time.February, 1, 1, 12, 0, 0, time.Local)
+	res = datetime.IsMidNightUTC(date)
+	assert.True(t, res)
+
+	date = time.Date(2020, time.February, 14, 0, 35, 0, 0, time.UTC)
+	res = datetime.IsMidNightUTC(date)
+	assert.True(t, res)
+}
+
+func TestLastMonth(t *testing.T) {
+	t.Parallel()
+
+	date := time.Date(2020, time.February, 14, 12, 12, 12, 0, time.Local)
+	expected := time.Date(2020, time.January, 1, 0, 0, 0, 0, time.UTC)
+	res := datetime.LastMonth(date)
+	assert.Equal(t, expected, res)
+
+	date = time.Date(2020, time.January, 14, 12, 12, 12, 0, time.Local)
+	expected = time.Date(2019, time.December, 1, 0, 0, 0, 0, time.UTC)
+	res = datetime.LastMonth(date)
+	assert.Equal(t, expected, res)
+}
+
+func TestThisMonth(t *testing.T) {
+	t.Parallel()
+
+	date := time.Date(2020, time.February, 14, 12, 12, 12, 0, time.Local)
+	expected := time.Date(2020, time.February, 1, 0, 0, 0, 0, time.UTC)
+	res := datetime.ThisMonth(date)
+	assert.Equal(t, expected, res)
+
+	date = time.Date(2020, time.January, 14, 12, 12, 12, 0, time.Local)
+	expected = time.Date(2020, time.January, 1, 0, 0, 0, 0, time.UTC)
+	res = datetime.ThisMonth(date)
+	assert.Equal(t, expected, res)
+}
+
+func TestNextMonth(t *testing.T) {
+	t.Parallel()
+
+	date := time.Date(2020, time.February, 14, 12, 12, 12, 0, time.Local)
+	expected := time.Date(2020, time.March, 1, 0, 0, 0, 0, time.UTC)
+	res := datetime.NextMonth(date)
+	assert.Equal(t, expected, res)
+
+	date = time.Date(2019, time.December, 14, 12, 12, 12, 0, time.Local)
+	expected = time.Date(2020, time.January, 1, 0, 0, 0, 0, time.UTC)
+	res = datetime.NextMonth(date)
+	assert.Equal(t, expected, res)
+}
+
+func TestLastQuarter(t *testing.T) {
+	t.Parallel()
+
+	date := time.Date(2020, time.February, 14, 12, 12, 12, 0, time.Local)
+	expected := time.Date(2019, time.October, 1, 0, 0, 0, 0, time.UTC)
+	res := datetime.LastQuarter(date)
+	assert.Equal(t, expected, res)
+}
+
+func TestThisQuarter(t *testing.T) {
+	t.Parallel()
+
+	date := time.Date(2020, time.February, 14, 12, 12, 12, 0, time.Local)
+	expected := time.Date(2020, time.January, 1, 0, 0, 0, 0, time.UTC)
+	res := datetime.ThisQuarter(date)
+	assert.Equal(t, expected, res)
+
+	date = time.Date(2020, time.January, 2, 12, 12, 12, 0, time.Local)
+	expected = time.Date(2020, time.January, 1, 0, 0, 0, 0, time.UTC)
+	res = datetime.ThisQuarter(date)
+	assert.Equal(t, expected, res)
+}
+
+func TestNextQuarter(t *testing.T) {
+	t.Parallel()
+
+	date := time.Date(2020, time.February, 14, 12, 12, 12, 0, time.Local)
+	expected := time.Date(2020, time.April, 1, 0, 0, 0, 0, time.UTC)
+	res := datetime.NextQuarter(date)
+	assert.Equal(t, expected, res)
+
+	date = time.Date(2020, time.December, 14, 12, 12, 12, 0, time.Local)
+	expected = time.Date(2021, time.January, 1, 0, 0, 0, 0, time.UTC)
+	res = datetime.NextQuarter(date)
+	assert.Equal(t, expected, res)
+}
+
+func TestLastYear(t *testing.T) {
+	t.Parallel()
+
+	date := time.Date(2020, time.February, 14, 12, 12, 12, 0, time.Local)
+	expected := time.Date(2019, time.January, 1, 0, 0, 0, 0, time.UTC)
+	res := datetime.LastYear(date)
+	assert.Equal(t, expected, res)
+}
+
+func TestThisYear(t *testing.T) {
+	t.Parallel()
+
+	date := time.Date(2020, time.February, 14, 12, 12, 12, 0, time.Local)
+	expected := time.Date(2020, time.January, 1, 0, 0, 0, 0, time.UTC)
+	res := datetime.ThisYear(date)
+	assert.Equal(t, expected, res)
+}
+
+func TestNextYear(t *testing.T) {
+	t.Parallel()
+
+	date := time.Date(2020, time.February, 14, 12, 12, 12, 0, time.Local)
+	expected := time.Date(2021, time.January, 1, 0, 0, 0, 0, time.UTC)
+	res := datetime.NextYear(date)
+	assert.Equal(t, expected, res)
+}
+
+func TestIsBeginingMonth(t *testing.T) {
+	t.Parallel()
+
+	date := time.Date(2020, time.January, 1, 12, 12, 12, 0, time.UTC)
+	res := datetime.IsBeginingMonth(date)
+	assert.True(t, res)
+
+	date = time.Date(2020, time.February, 1, 12, 12, 12, 0, time.UTC)
+	res = datetime.IsBeginingMonth(date)
+	assert.True(t, res)
+
+	date = time.Date(2020, time.September, 2, 12, 12, 12, 0, time.UTC)
+	res = datetime.IsBeginingMonth(date)
+	assert.False(t, res)
+}
+
+func TestIsBeginingQuarter(t *testing.T) {
+	t.Parallel()
+
+	date := time.Date(2020, time.January, 1, 0, 0, 0, 0, time.UTC)
+	res := datetime.IsBeginingQuarter(date)
+	assert.True(t, res)
+
+	date = time.Date(2020, time.April, 1, 0, 0, 0, 0, time.UTC)
+	res = datetime.IsBeginingQuarter(date)
+	assert.True(t, res)
+
+	date = time.Date(2020, time.July, 1, 0, 0, 0, 0, time.UTC)
+	res = datetime.IsBeginingQuarter(date)
+	assert.True(t, res)
+
+	date = time.Date(2020, time.October, 1, 0, 0, 0, 0, time.UTC)
+	res = datetime.IsBeginingQuarter(date)
+	assert.True(t, res)
+
+	date = time.Date(2020, time.February, 1, 0, 0, 0, 0, time.UTC)
+	res = datetime.IsBeginingQuarter(date)
+	assert.False(t, res)
+
+	date = time.Date(2020, time.April, 11, 0, 0, 0, 0, time.UTC)
+	res = datetime.IsBeginingQuarter(date)
+	assert.False(t, res)
+}
+
+func TestIsBeginingYear(t *testing.T) {
+	t.Parallel()
+
+	date := time.Date(2020, time.January, 1, 12, 12, 12, 0, time.UTC)
+	res := datetime.IsBeginingYear(date)
+	assert.True(t, res)
+
+	date = time.Date(2020, time.February, 1, 12, 12, 12, 0, time.UTC)
+	res = datetime.IsBeginingYear(date)
+	assert.False(t, res)
+
+	date = time.Date(2020, time.September, 11, 12, 12, 12, 0, time.UTC)
+	res = datetime.IsBeginingYear(date)
+	assert.False(t, res)
+}
